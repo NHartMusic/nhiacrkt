1093039c292b793b3f502f2caf9e17ec4476c3c3
Nick Hart <nhartmusic@gmail.com>
2020-12-11 18:08:31 +0100

    was going to set up firebase with a database set to native for europe and then I remembered google domains only work in the US so I will have to do this whole process again later I suppose

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,16 +1,15 @@
-// import firebase from 'firebase/app'
+import firebase from 'firebase/app'
 
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBPY21IJSrHgjNMe_XZic0iN6q4zddE1IE",
-//     authDomain: "kieku-5f953.firebaseapp.com",
-//     databaseURL: "https://kieku-5f953.firebaseio.com",
-//     projectId: "kieku-5f953",
-//     storageBucket: "kieku-5f953.appspot.com",
-//     messagingSenderId: "779108296473",
-//     appId: "1:779108296473:web:5155618dca6f31ade7c788",
-//     measurementId: "G-0YBQQ1WKN6"
-//   };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9TdkDDVOtz4EkNxhqAnpdzK1Ul_lYOy4",
+    authDomain: "nhiacrkt-83da3.firebaseapp.com",
+    projectId: "nhiacrkt-83da3",
+    storageBucket: "nhiacrkt-83da3.appspot.com",
+    messagingSenderId: "440811810156",
+    appId: "1:440811810156:web:509cae441e5a5ad17a5749",
+    measurementId: "G-5YSXEVZY1L"
+};
 
-// firebase.initializeApp(firebaseConfig)
-// export default firebase
+firebase.initializeApp(firebaseConfig)
+export default firebase
