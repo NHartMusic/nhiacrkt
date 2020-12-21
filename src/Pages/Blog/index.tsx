cf933b9c58d088ef91de79220cbae799f343e230
Nick Hart <nhartmusic@gmail.com>
2020-12-21 18:11:26 +0100

    album list coming together

--- src/Pages/Blog/index.tsx
+++ src/Pages/Blog/index.tsx
@@ -3,7 +3,7 @@ import React, { PureComponent, ReactNode } from 'react'
 import { Title } from '../../UI/Typography'
 import { fade0 } from '../../UI/Media'
 import { TextWrapperCenter, FadeWrapperBlock } from '../../UI/Containers'
-import ExpansionPanel from '../../Components/ExpansionPanel'
+import TopAlbums from '../../Components/TopAlbums'
 
 interface Props { }
 interface State { }
@@ -29,7 +29,7 @@ class Blog extends PureComponent<Props, State> {
                         <Title>Blog</Title>
                     </TextWrapperCenter>
                 </FadeWrapperBlock>
-                <ExpansionPanel />
+                <TopAlbums />
             </>
         )
     }
--- src/UI/Typography.ts
+++ src/UI/Typography.ts
@@ -2,16 +2,25 @@ import styled from 'styled-components'
 import { Blues, Greens, Greyscale } from './Colors'
 import { Link } from 'react-router-dom'
 
-export const ParagraphLarge = styled.h3`
+export const ParagraphLarge = styled.p`
     font-family: Sora;
     color: white;
     text-decoration: none;
+    font-size: 1.2em;
 `
 
 export const ParagraphRegular = styled.p`
     font-family: 'Roboto', sans-serif;
-    font-size: 16px;
+    font-size: 1.0em;
+`
+
+export const SubTitle = styled.p`
+    display: flex;
+    margin: 0 auto;
+    font-family: Sora;
     color: white;
+    text-decoration: none;
+    font-size: 1.3em;
 `
 
 export const TextLink = styled(Link)`
