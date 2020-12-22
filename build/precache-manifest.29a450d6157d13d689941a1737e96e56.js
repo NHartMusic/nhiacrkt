468a35b0f00a64dc4608f1ca0a1b30f0d016f0cd
Nick Hart <nhartmusic@gmail.com>
2020-12-22 13:38:29 +0100

    firebase configured

---
+++ build/precache-manifest.29a450d6157d13d689941a1737e96e56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d799e8be0889a83d1db7185baceb4c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf695d04e53848a8a96",
+    "url": "/static/js/2.b04c202f.chunk.js"
+  },
+  {
+    "revision": "05542f73db2c31c5889e516d924a2952",
+    "url": "/static/js/2.b04c202f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd84190a4614ed75568",
+    "url": "/static/js/main.9b5041ed.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "e0d17fb58eaffeb3c0ba64a22fb97cb8",
+    "url": "/static/media/Golden_Gate.e0d17fb5.png"
+  },
+  {
+    "revision": "997680f555b197caf6ef5be9e8bd7a0e",
+    "url": "/static/media/Kieku_Pitch_Deck.997680f5.pdf"
+  }
+]);
