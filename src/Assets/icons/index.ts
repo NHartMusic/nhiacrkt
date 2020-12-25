e6bd3bd0c6fba374aac2171f18523d5cb1220abc
Nick Hart <nhartmusic@gmail.com>
2020-12-25 17:50:14 +0100

    icon bar set up

---
+++ src/Assets/icons/index.ts
@@ -0,0 +1,13 @@
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import linkedin from './linkedin.png'
+import twitter from './twitter.png'
+import upwork from './upwork.png'
+
+export {
+    facebook,
+    instagram,
+    linkedin,
+    twitter,
+    upwork
+}
