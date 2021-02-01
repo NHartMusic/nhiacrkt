75c0d7f0c845b09c8c130c94584eb3ed859a2099
Nick Hart <nhartmusic@gmail.com>
2021-02-01 16:52:07 +0100

    ok

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -48,14 +48,6 @@ export default function Home(): JSX.Element {
                     I worked in the music industry for over 5 years before that. I love designing websites and I
                     fucking hate the Grateful Dead.
                 </ParagraphLarge>
-
-                <ParagraphLarge>
-                    This website is a work in progress. The only reason I'm putting it up in such an unfinished manner
-                    is because I wanted to share my end of the year albums list with everyone before we reached 2021. I
-                    got a little sidetracked when the greatest rapper of all time was revealed to have passed away. Still
-                    fine tuning everything. Check back in a few days and this website will have a lot more content and
-                    look a whole lot more impressive.
-                </ParagraphLarge>
             </TextWrapperLeft>
 
 
